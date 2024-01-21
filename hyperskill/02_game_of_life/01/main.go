3692203b6c7782c6e4dbb48d0ac2f36913da39b6
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2024-01-21 21:23:35 +0100

    feat: complete 1st stage of the project

---
+++ hyperskill/02_game_of_life/01/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+)
+
+func handleError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func readLine() string {
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	handleError(err)
+	return strings.TrimSpace(text)
+}
+
+func readNAndSeed() (int, int) {
+	var n, seed int
+	fmt.Scanf("%d %d", &n, &seed)
+	return n, seed
+}
+
+type CellState int
+
+const (
+	CellStateDead CellState = iota
+	CellStateAlive
+)
+
+func generateBoard(n int) [][]CellState {
+	board := make([][]CellState, n)
+	for i := range board {
+		board[i] = make([]CellState, n)
+		for j := range board[i] {
+			board[i][j] = CellStateDead
+		}
+	}
+	return board
+}
+
+func initBoard(board [][]CellState, seed int) {
+	src := rand.NewSource(int64(seed))
+	r := rand.New(src)
+	for i := range board {
+		for j := range board[i] {
+			board[i][j] = CellState(r.Intn(2))
+		}
+	}
+}
+
+const (
+	CellSymbolDead  = " "
+	CellSymbolAlive = "O"
+)
+
+func printBoard(board [][]CellState) {
+	for i := range board {
+		for j := range board[i] {
+			if board[i][j] == CellStateAlive {
+				fmt.Print(CellSymbolAlive)
+			} else if board[i][j] == CellStateDead {
+				fmt.Print(CellSymbolDead)
+			} else {
+				fmt.Println("Unknown cell state")
+				os.Exit(1)
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func game() {
+	n, seed := readNAndSeed()
+	board := generateBoard(n)
+	initBoard(board, seed)
+	printBoard(board)
+}
+
+func main() {
+	game()
+}
