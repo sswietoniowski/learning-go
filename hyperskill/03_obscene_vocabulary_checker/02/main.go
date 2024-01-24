5cbfdc386b126766264288c20367e5825dc443fb
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2024-01-24 09:05:56 +0100

    feat: complete 2nd stage of the project

---
+++ hyperskill/03_obscene_vocabulary_checker/02/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type vocabulary struct {
+	words map[string]bool
+}
+
+func newVocabulary() *vocabulary {
+	return &vocabulary{words: make(map[string]bool)}
+}
+
+func cleanWord(word string) string {
+	return strings.ToLower(strings.TrimSpace(word))
+}
+
+func (v *vocabulary) addWord(word string) {
+	v.words[cleanWord(word)] = true
+}
+
+func handleErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (v *vocabulary) readWords(fileName string) {
+	file, err := os.Open(fileName)
+	handleErr(err)
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		word := scanner.Text()
+		v.addWord(word)
+	}
+}
+
+func (v *vocabulary) isTaboo(word string) bool {
+	return v.words[cleanWord(word)]
+}
+
+func checker() {
+	readLine := func() string {
+		var line string
+
+		_, err := fmt.Scan(&line)
+		handleErr(err)
+
+		return line
+	}
+
+	fileName := readLine()
+
+	vocabulary := newVocabulary()
+	vocabulary.readWords(fileName)
+
+	word := readLine()
+
+	if vocabulary.isTaboo(word) {
+		fmt.Println("True")
+	} else {
+		fmt.Println("False")
+	}
+}
+
+func main() {
+	checker()
+}
