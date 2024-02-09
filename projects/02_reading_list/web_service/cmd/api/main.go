15739c815878eb3aec09814dda50d2e699c75d6c
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2024-02-09 10:39:16 +0100

    refactor: code refactor

--- projects/02_reading_list/web_service/cmd/api/main.go
+++ projects/02_reading_list/web_service/cmd/api/main.go
@@ -19,6 +19,11 @@ func main() {
 }
 
 func healthcheck(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
 	fmt.Fprintln(w, "status: available")
 	fmt.Fprintf(w, "environment: %s\n", "development")
 	fmt.Fprintf(w, "version: %s\n", "1.0.0")
