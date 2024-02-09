9e6a1a32e4aa7c3766793964903bbdf1a64cdb9d
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2024-02-09 10:32:41 +0100

    feat: create health check endpoint

--- projects/02_reading_list/web_service/cmd/api/main.go
+++ projects/02_reading_list/web_service/cmd/api/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	// TODO: Implement the main function
+	http.HandleFunc("/api/v1/healthcheck", healthcheck)
+
+	fmt.Println("Server is running on port 4000")
+
+	err := http.ListenAndServe(":4000", nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "status: available")
+	fmt.Fprintf(w, "environment: %s\n", "development")
+	fmt.Fprintf(w, "version: %s\n", "1.0.0")
 }
