9f71f5c2eecdbebed00d3c395ab3c895de05ad76
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2024-02-11 07:53:51 +0100

    fix: correct db initialization script

--- projects/02_reading_list/web_service/scripts/setup.sql
+++ projects/02_reading_list/web_service/scripts/setup.sql
@@ -12,7 +12,9 @@ CREATE TABLE books (
 );
 
 INSERT INTO books
-(id, title, author, published, pages, genres, rating, version, read)
+(id, title, author, published, pages, genres, rating, version, "read", created_at)
 VALUES
-("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 1979, 224, {"comedy", "science fiction"}, 5.0, 1, false, "2024-01-01 08:00:00"),
-("The Hobbit", "J.R.R. Tolkien", 1937, 310, {"adventure", "fantasy"}, 4.5, 1, true, "2024-01-01 09:00:00");
+(1, 'The Hitchhiker''s Guide to the Galaxy', 'Douglas Adams', 1979, 224, ARRAY['comedy', 'science fiction'], 5.0, 1, false, '2024-01-01 08:00:00'),
+(2, 'The Hobbit', 'J.R.R. Tolkien', 1937, 310, ARRAY['adventure', 'fantasy'], 4.5, 1, true, '2024-01-01 09:00:00');
+
+SELECT setval('books_id_seq', (SELECT MAX(id) FROM books));
