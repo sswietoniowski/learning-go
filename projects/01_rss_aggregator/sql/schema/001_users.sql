6d9e84a534101d739b2e51f8d627bc14b700bb60
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2024-03-01 12:54:31 +0100

    refactor: reorganize SQL

--- projects/01_rss_aggregator/sql/schema/001_users.sql
+++ projects/01_rss_aggregator/sql/schema/001_users.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 CREATE TABLE users (
     id UUID PRIMARY KEY,
-    name TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL
 );
 
 -- +goose Down
