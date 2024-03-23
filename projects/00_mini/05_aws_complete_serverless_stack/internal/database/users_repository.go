e26d0bad3457900ff2063265e83839a06da68351
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2024-03-23 19:55:35 +0100

    feat: partially implement repository

--- projects/00_mini/05_aws_complete_serverless_stack/internal/database/users_repository.go
+++ projects/00_mini/05_aws_complete_serverless_stack/internal/database/users_repository.go
@@ -1,9 +1,25 @@
 package database
 
 import (
+	"errors"
+
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var (
+	ErrorFailedToUnmarshalRecord = "failed to unmarshal record"
+	ErrorFailedToFetchRecord     = "failed to fetch record"
+	ErrorInvalidUserData         = "invalid user data"
+	ErrorInvalidEmail            = "invalid email"
+	ErrorCouldNotMarshalItem     = "could not marshal item"
+	ErrorCouldNotDeleteItem      = "could not delete item"
+	ErrorCouldNotDynamoPutItem   = "could not dynamo put item"
+	ErrorUserAlreadyExists       = "user.User already exists"
+	ErrorUserDoesNotExist        = "user.User does not exist"
 )
 
 type UsersRepository struct {
@@ -23,7 +39,44 @@ func NewUsersRepository(session *session.Session) *UsersRepository {
 }
 
 func (ur *UsersRepository) GetUserByEmail(email string) (*User, error) {
-	return nil, nil
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"email": {
+				S: aws.String(email),
+			},
+		},
+		TableName: aws.String(ur.tableName),
+	}
+
+	result, err := ur.dynaClient.GetItem(input)
+	if err != nil {
+		return nil, errors.New(ErrorFailedToFetchRecord)
+	}
+
+	user := new(User)
+	if err := dynamodbattribute.UnmarshalMap(result.Item, user); err != nil {
+		return nil, errors.New(ErrorFailedToUnmarshalRecord)
+	}
+
+	return user, nil
+}
+
+func (ur *UsersRepository) GetUsers() ([]User, error) {
+	input := &dynamodb.ScanInput{
+		TableName: aws.String(ur.tableName),
+	}
+
+	result, err := ur.dynaClient.Scan(input)
+	if err != nil {
+		return nil, errors.New(ErrorFailedToFetchRecord)
+	}
+
+	users := make([]User, 0)
+	if err := dynamodbattribute.UnmarshalListOfMaps(result.Items, users); err != nil {
+		return nil, errors.New(ErrorFailedToUnmarshalRecord)
+	}
+
+	return users, nil
 }
 
 func (ur *UsersRepository) CreateUser(user *User) error {
